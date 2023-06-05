7e900f2b8c6cb0357864cf84ce27d6fda7622409
DML <djamel.dyne.gaming@gmail.com>
2023-06-06 00:23:20 +0200

    first commit

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,21 @@
+<script>
+  import white from '../../images/white.svg'
+  import black from '../../images/black.svg'
+  import trad from '../../images/trad.svg'
+</script>
+
+<navbar class="navbar">
+  <div class="theme-container">
+    <img src={black} alt="Black">
+  </div>
+  <div class="txt-container">
+
+  </div>
+  <div class="trad-container">
+    <img src={trad} alt="Traduction">
+  </div>
+</navbar>
+
+<style>
+  @import './Navbar.css';
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Navbar from '../lib/components/navbar/Navbar.svelte'; 
+</script>
+
+<svelte:head>
+	<title>DML - Qui suis-je</title>
+	<meta name="description" content="Site officiel de DML, dessus vous pourrez retrouver des créations Web, Bot Discord et Add-Ons hors normes !" />
+	<meta content="#121315" data-react-helmet="true" name="theme-color"/>
+	<meta property="og:image" content="https://i.ibb.co/jTbFfFX/favicon.jpg" />
+</svelte:head>
+
+<Navbar />
+
+<style>
+	@import '../styles.css';
+</style>

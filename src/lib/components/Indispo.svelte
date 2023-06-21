20e63d7991d3afc53b33e0a7a533767f6fb79696
DML <djamel.dyne.gaming@gmail.com>
2023-06-21 20:37:52 +0200

    code push

---
+++ src/lib/components/Indispo.svelte
@@ -0,0 +1,7 @@
+<indispo class="indispo">
+  <h9>Page Indisponible pour le moment !<br>En cours de création...</h9>
+</indispo>
+
+<style>
+  @import './Indispo.css';
+</style>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -8,7 +8,7 @@
 
 <svelte:head>
 	<title>DML - Portfolio</title>
-	<meta name="description" content="Site officiel de DML, dessus vous pourrez retrouver des créations Web, Bot Discord et Add-Ons hors normes !" />
+	<meta name="description" content="Je m'appelle Djamel, j'ai 16 ans et je réside en région parisienne. Depuis mon plus jeune âge, ma passion pour l'informatique ne cesse de grandir. Au cours de la dernière année, j'ai particulièrement plongé dans le domaine du Développement Web et je me suis concentré sur l'apprentissage approfondi du Framework Svelte. Mais je fais aussi des Bots Discord en utilisant le langage de développement TypeScript et des Add-Ons Minecraft Bedrock en utilisant JSON et JavaScript." />
 	<meta content="#121315" data-react-helmet="true" name="theme-color"/>
 	<meta property="og:image" content="https://i.ibb.co/jTbFfFX/favicon.jpg" />
 </svelte:head>

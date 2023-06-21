68bae8148283703db635e36d08b0c51d0ed66132
DML <djamel.dyne.gaming@gmail.com>
2023-06-21 20:00:48 +0200

    code push

--- src/lib/components/portfolio/avis/Avis.svelte
+++ src/lib/components/portfolio/avis/Avis.svelte
@@ -10,6 +10,8 @@
   <div class="left-arrow">
     <img src={left} alt="Flèche de Gauche">
   </div>
+  <div class="avis-title">
+    <h1>Avis :</h1>
   <div class="avis-container">
     <div class="header-container">
       <img src={pdp} alt="PDP">
@@ -19,6 +21,7 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla erat ac fermentum posuere. Nulla et nunc id dolor tincidunt viverra. Quisque efficitur tortor a dui congue, vitae lacinia mi tincidunt. Fusce nec malesuada velit. Sed eu metus id augue auctor sollicitudin a eget lorem. Etiam semper tristique neque, non faucibus urna euismod ut. Nullam bibendum quam at convallis auctor. Donec nec tortor eget purus tristique rhoncus. Vestibulum varius dui non massa rutrum aliquet. Nunc sit amet enim tincidunt, venenatis mauris eget, dignissim elit. Pellentesque quis enim non tortor efficitur venenatis. Nullam pulvinar, elit sed luctus consequat, ex nibh facilisis est, at pharetra dui felis at orci. Fusce vel rutrum velit. Nullam tristique mauris vitae dolor consequat condimentum. In tempor nisl et ex semper, non pellentesque erat tincidunt.</p>
     </div>
   </div>
+</div>
   <div class="right-arrow">
     <img src={right} alt="Flèche de Droite">
   </div>
